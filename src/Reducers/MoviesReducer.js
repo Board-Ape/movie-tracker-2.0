5b933299e9e57a97d7673591d358d21c6335a75a
David Ryan <djrdev@icloud.com>
2017-12-15 20:23:17 -0700

    initial consolidation

---
+++ src/Reducers/MoviesReducer.js
@@ -0,0 +1,45 @@
+
+
+const initialState = {
+  movies: [],
+  moviesHasErrored: false,
+  moviesIsLoading: false
+}
+
+
+
+const movies = (state = initialState, action) => {
+  switch (action.type) {
+    case 'MOVIES_FETCH_DATA_SUCCESS':
+      return action.movies.results;
+
+    default:
+      return state;
+  }
+}
+
+export default movies;
+
+const moviesHasErrored = (state = initialState, action) => {
+  switch (action.type) {
+    case 'MOVIES_HAS_ERRORED':
+      return action.hasErrored;
+
+    default:
+      return state;
+  }
+}
+
+export default moviesHasErrored;
+
+const moviesIsLoading = (state = initialState, action) => {
+  switch (action.type) {
+    case 'MOVIES_IS_LOADING':
+      return action.isLoading;
+
+    default:
+      return state;
+  }
+}
+
+export default moviesIsLoading;
